495173bfeaded3c4b1ee05217308f6cdc079769a
Gaurav Poudel <gauravpaudel2013@gmail.com>
2021-12-11 23:33:26 +0545

    Added line

--- EntryForm.Designer.cs
+++ EntryForm.Designer.cs
@@ -39,13 +39,21 @@ namespace CourseworkAD
             this.typeValue = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.idNumericUpDownValue)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.countNumericUpDownValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Location = new System.Drawing.Point(148, 129);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(524, 288);
@@ -86,7 +94,7 @@ namespace CourseworkAD
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(478, 70);
+            this.label3.Location = new System.Drawing.Point(478, 67);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 30);
             this.label3.TabIndex = 4;
@@ -95,7 +103,7 @@ namespace CourseworkAD
             // countNumericUpDownValue
             // 
             this.countNumericUpDownValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.countNumericUpDownValue.Location = new System.Drawing.Point(572, 75);
+            this.countNumericUpDownValue.Location = new System.Drawing.Point(592, 72);
             this.countNumericUpDownValue.Name = "countNumericUpDownValue";
             this.countNumericUpDownValue.Size = new System.Drawing.Size(66, 22);
             this.countNumericUpDownValue.TabIndex = 5;
@@ -110,7 +118,7 @@ namespace CourseworkAD
             "Adult",
             "Group",
             "Staff"});
-            this.typeValue.Location = new System.Drawing.Point(376, 70);
+            this.typeValue.Location = new System.Drawing.Point(386, 70);
             this.typeValue.Name = "typeValue";
             this.typeValue.Size = new System.Drawing.Size(73, 28);
             this.typeValue.TabIndex = 6;
@@ -120,7 +128,7 @@ namespace CourseworkAD
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(13, 9);
+            this.label4.Location = new System.Drawing.Point(1, -1);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(187, 45);
             this.label4.TabIndex = 7;
@@ -140,12 +148,67 @@ namespace CourseworkAD
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(524, 235);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(172, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(354, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(546, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(0, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(188, 37);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "_________";
+            // 
             // EntryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.typeValue);
@@ -155,12 +218,15 @@ namespace CourseworkAD
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label8);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "EntryForm";
             this.Text = "EntryForm";
             this.Load += new System.EventHandler(this.EntryForm_Load);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.idNumericUpDownValue)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.countNumericUpDownValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,5 +244,10 @@ namespace CourseworkAD
         private System.Windows.Forms.ComboBox typeValue;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
