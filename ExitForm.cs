eac374a6b402636e85c68546687c0cd6bfc4f2c0
Gaurav Poudel <gauravpaudel2013@gmail.com>
2021-12-11 22:28:19 +0545

    Forms Added

---
+++ ExitForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseworkAD
+{
+    public partial class ExitForm : Form
+    {
+        public ExitForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
