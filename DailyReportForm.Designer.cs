195d9b5161684c65717c68602aafd919c19238cf
Gaurav Poudel <gauravpaudel2013@gmail.com>
2021-12-11 22:45:39 +0545

    Added forms

--- DailyReportForm.Designer.cs
+++ DailyReportForm.Designer.cs
@@ -29,10 +29,19 @@ namespace CourseworkAD
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // DailyReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DailyReportForm";
             this.Text = "DailyReportForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
